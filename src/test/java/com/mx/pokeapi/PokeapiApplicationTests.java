1f33ea292ccd5f589cf54f507c108098e9115a9c
Luis Gerardo <lgjc1im11@gmail.com>
2024-05-14 01:06:46 -0600

    Readme update

--- src/test/java/com/mx/pokeapi/PokeapiApplicationTests.java
+++ src/test/java/com/mx/pokeapi/PokeapiApplicationTests.java
@@ -1,13 +1,47 @@
 package com.mx.pokeapi;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mx.pokeapi.dto.DetailsDTO;
+import com.mx.pokeapi.dto.RecordsDTO;
+import com.mx.pokeapi.exception.PokeapiException;
+import com.mx.pokeapi.interfaces.PokemonInterface;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class PokeapiApplicationTests {
+	
+	@Autowired
+	private PokemonInterface pokemonInterface;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void getPokemons() {
+		try {
+			RecordsDTO recordsDTO = this.pokemonInterface.getPokemons("192.168.200.100", new Date());
+			log.info("Data -> "+recordsDTO);
+		} catch (PokeapiException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void getPokemonsByName() {
+		try {
+			DetailsDTO detailsDTO = this.pokemonInterface.getPokemonByName("ditto","192.168.200.100", new Date());
+			log.info("Data -> "+detailsDTO);
+		} catch (PokeapiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
